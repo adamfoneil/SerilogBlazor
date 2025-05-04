c7a2e9c4fb3d0c315c4b3aa25c0e1b8a6c1bc3c6
Adam O'Neil <adamosoftware@gmail.com>
2025-05-03 20:25:00 -0400

    starting on sandbox console app

---
+++ SandboxCmd/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Service.IndexedLogContext;
+
+namespace SandboxCmd;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IIndexedLogContext
+{
+	public DbSet<ExceptionTemplate> ExceptionTemplates { get; set; }
+	public DbSet<ExceptionInstance> ExceptionInstances { get; set; }
+}
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+	public const string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=SerilogViewerDemo;Integrated Security=true;TrustServerCertificate=True;";
+
+	public ApplicationDbContext CreateDbContext(string[] args)
+	{
+		var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+		optionsBuilder.UseSqlServer(ConnectionString);
+		return new ApplicationDbContext(optionsBuilder.Options);
+	}
+}
---
+++ SandboxCmd/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SandboxCmd;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+
+var host = Host.CreateDefaultBuilder(args)
+	.UseSerilog((context, services, configuration) =>
+	{
+		configuration
+			.WriteTo.Console()
+			.WriteTo.MSSqlServer(AppDbContextFactory.ConnectionString, new MSSqlServerSinkOptions()
+			{
+				AutoCreateSqlTable = true,
+				TableName = "Serilog",
+				SchemaName = "log"
+			});		
+	})
+	.ConfigureServices((context, services) =>
+	{
+		services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(AppDbContextFactory.ConnectionString));
+	})
+	.Build();
+
+await host.RunAsync();
