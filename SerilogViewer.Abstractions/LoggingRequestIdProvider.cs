7d941a4354b58b10c2b2686fb7803d98fbf9f4e3
Adam O'Neil <adamosoftware@gmail.com>
2025-06-26 06:04:01 -0400

    scope request increment to the date

--- SerilogViewer.Abstractions/LoggingRequestIdProvider.cs
+++ SerilogViewer.Abstractions/LoggingRequestIdProvider.cs
@@ -7,10 +7,18 @@
 public class LoggingRequestIdProvider
 {
 	private int _currentId = 0;
+	private DateOnly _date = DateOnly.FromDateTime(DateTime.UtcNow);
 
 	public string NextId()
 	{
+		var today = DateOnly.FromDateTime(DateTime.UtcNow);
+		if (today > _date)
+		{
+			_date = today;
+			_currentId = 0;
+		}
+
 		var id = Interlocked.Increment(ref _currentId);
-		return $"request-{id}";
+		return $"{_date:yyyMMdd}-{id:0000000}";
 	}
 }
