ad8b3d4c18949966731e1207422e78ec4ac64bbc
Adam O'Neil <adamosoftware@gmail.com>
2025-06-28 16:46:15 -0400

    move ParseAgeText to a higher level, also set user name

--- SerilogViewer.SqlServer/SerilogSqlServerQuery.cs
+++ SerilogViewer.SqlServer/SerilogSqlServerQuery.cs
@@ -28,6 +28,7 @@ public class SerilogSqlServerQuery(
 		public string Level { get; init; } = default!;
 		public string MessageTemplate { get; init; } = default!;
 		public string Message { get; init; } = default!;
+		public string? UserName { get; init; }
 		public string? Exception { get; init; }
 		public string? PropertyXml { get; init; }		
 	}
@@ -43,7 +44,7 @@ public class SerilogSqlServerQuery(
 			@$"SELECT 
 				[Id], [Timestamp], DATEDIFF(n, [Timestamp], {CurrentTimeFunction(TimestampType)}) AS [AgeMinutes], 
 				[SourceContext], [RequestId], [Level], [MessageTemplate], 
-				[Message], [Exception], [Properties] AS [PropertyXml]
+				[Message], [Exception], [Properties] AS [PropertyXml], [UserName]
 			FROM [{_schemaName}].[{_tableName}] 
 			{whereClause} 
 			ORDER BY [Timestamp] DESC 
@@ -86,39 +87,20 @@ public class SerilogSqlServerQuery(
 	{
 		Id = source.Id,
 		Timestamp = source.Timestamp,
-		AgeText = ParseAgeText(source.AgeMinutes),
+		AgeText = DateHelper.ParseAgeText(source.AgeMinutes),
 		SourceContext = source.SourceContext,
 		RequestId = source.RequestId,
 		Level = source.Level,
 		MessageTemplate = source.MessageTemplate,
 		Message = source.Message,
-		Exception = source.Exception,
+		Exception = source.Exception,		
+		UserName = source.UserName,
 		Properties = string.IsNullOrEmpty(source.PropertyXml)
 			? []
 			: System.Xml.Linq.XElement.Parse(source.PropertyXml)
 				.Elements()
 				.ToDictionary(e => e.Attribute("key")!.Value, e => (object)e.Value)
-	};
-
-	public static string ParseAgeText(int ageMinutes)
-	{
-		if (ageMinutes < 1)
-			return "just now";
-
-		var ts = TimeSpan.FromMinutes(ageMinutes);
-
-		var parts = new List<string>();
-
-		if (ts.Days > 0)
-			parts.Add($"{ts.Days}d");
-		if (ts.Hours > 0)
-			parts.Add($"{ts.Hours}h");
-		if (ts.Minutes > 0 && ts.Days == 0) // Only show minutes if less than a day
-			parts.Add($"{ts.Minutes}m");
-
-		return string.Join(", ", parts);
-	}
-
+	};	
 
 	private static string CurrentTimeFunction(TimestampType timestampType) => timestampType switch
 	{
