d8aa4f55998aaa2c0021188e5d3b5f10afece713
Adam O'Neil <adamosoftware@gmail.com>
2025-06-29 07:51:58 -0400

    comment on how this is done

--- SampleApp/ApplicationLogLevels.cs
+++ SampleApp/ApplicationLogLevels.cs
@@ -6,6 +6,10 @@ namespace SampleApp;
 
 public class ApplicationLogLevels : LogLevels
 {
+	/// <summary>
+	/// Set your defaults as a field initialized once so that values can be changed at runtime.
+	/// Changes at runtime won't persist across app restarts, but will allow for dynamic adjustments during an app's lifetime.
+	/// </summary>
 	private readonly Dictionary<string, LoggingLevelSwitch> _loggingLevels = new()
 	{
 		["System"] = new(LogEventLevel.Warning),
