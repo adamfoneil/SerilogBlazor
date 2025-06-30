789d4bd1170bddfaee4faaca9199a33a9ea6b56e
Adam O'Neil <adamosoftware@gmail.com>
2025-06-30 06:56:44 -0400

    don't need debug level by default

--- SampleApp/ApplicationLogLevels.cs
+++ SampleApp/ApplicationLogLevels.cs
@@ -14,8 +14,8 @@ public class ApplicationLogLevels : LogLevels
 	{
 		["System"] = new(LogEventLevel.Warning),
 		["Microsoft"] = new(LogEventLevel.Warning),
-		["SampleApp"] = new(LogEventLevel.Debug),
-		["SerilogViewer.SqlServer"] = new(LogEventLevel.Debug)
+		["SampleApp"] = new(LogEventLevel.Information),
+		["SerilogViewer.SqlServer"] = new(LogEventLevel.Information)
 	};
 
 	public override Dictionary<string, LoggingLevelSwitch> LoggingLevels => _loggingLevels;
