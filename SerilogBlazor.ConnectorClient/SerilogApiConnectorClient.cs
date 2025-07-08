82a3533f9e0de08a4d8bceb1f4343792d12688b9
Adam O'Neil <adamosoftware@gmail.com>
2025-07-08 07:53:26 -0400

    started on connector client

---
+++ SerilogBlazor.ConnectorClient/SerilogApiConnectorClient.cs
@@ -0,0 +1,20 @@
+﻿using SerilogBlazor.Abstractions;
+
+namespace SerilogBlazor.ConnectorClient;
+
+public class SerilogApiConnectorClient(IHttpClientFactory httpClientFactory)
+{
+	private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+
+	public async Task<SerilogEntry[]> QueryEntriesAsync(string endpoint, string headerSecret, string? criteria = null)
+	{
+		throw new NotImplementedException();
+	}
+
+	public async Task<SourceContextMetricsResult[]> QueryMetricsAsync(string endpoint, string headerSecret)
+	{
+		throw new NotImplementedException();
+	}
+
+	// todo: log levels
+}
