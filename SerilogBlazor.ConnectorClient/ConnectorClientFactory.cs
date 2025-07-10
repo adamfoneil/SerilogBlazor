fffadb2b6cbb4d744fa4e4420e108084868c4b44
Adam O'Neil <adamosoftware@gmail.com>
2025-07-10 05:50:26 -0400

    v1.0.13 factory

---
+++ SerilogBlazor.ConnectorClient/ConnectorClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SerilogBlazor.ConnectorClient;
+
+public class ConnectorClientFactory(IServiceProvider serviceProvider)
+{
+	private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+	public SerilogApiConnectorClient CreateClient(string endpoint, string headerSecret)
+	{
+		using var scope = _serviceProvider.CreateScope();
+		var httpClientFactory = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>();
+		var logger = scope.ServiceProvider.GetRequiredService<ILogger<SerilogApiConnectorClient>>();
+		return new SerilogApiConnectorClient(logger, httpClientFactory, endpoint, headerSecret);
+	}
+}
---
+++ SerilogBlazor.ConnectorClient/StartupExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SerilogBlazor.ConnectorClient;
+
+public static class StartupExtensions
+{
+	public static void AddSerilogConnectorClientFactory(this IServiceCollection services)
+	{
+		services.AddHttpClient();
+		services.AddSingleton<ConnectorClientFactory>();
+	}
+}
